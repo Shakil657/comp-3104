545e24608db22c8c0b1fd2e7e26dc13dd5c0f444
Shakil <shakil_miah@hotmail.com>
2022-10-06 18:44:25 -0400

    website added

---
+++ build/precache-manifest.6d8784f4d2ff4f76270da10c9418273c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab754b34416f2dd52bcd",
+    "url": "/Shakil657/comp-3104/static/css/main.e4e273a9.chunk.css"
+  },
+  {
+    "revision": "ab754b34416f2dd52bcd",
+    "url": "/Shakil657/comp-3104/static/js/main.ab754b34.chunk.js"
+  },
+  {
+    "revision": "2e194f0f5e102749938d",
+    "url": "/Shakil657/comp-3104/static/js/runtime~main.2e194f0f.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Shakil657/comp-3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "1f85bd88dd13aa43a848f3542bbbcd44",
+    "url": "/Shakil657/comp-3104/index.html"
+  }
+];
